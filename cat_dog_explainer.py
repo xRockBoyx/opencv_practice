2134a31cf6c81308087528b6a3233d0c630c8203
xRockBoyx <kp3344567@gmail.com>
2022-12-14 00:32:04 +0800

    Cat_dog_training

--- cat_dog_explainer.py
+++ cat_dog_explainer.py
@@ -63,19 +63,20 @@ score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+model.save('a.h5')
 # ...include code from https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
 
-import shap
-import numpy as np
+# import shap
+# import numpy as np
 
-# select a set of background examples to take an expectation over
-background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)]
+# # select a set of background examples to take an expectation over
+# background = x_train[np.random.choice(x_train.shape[0], 100, replace=False)]
 
-# explain predictions of the model on three images
-e = shap.DeepExplainer(model, background)
-# ...or pass tensors directly
-# e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), background)
-shap_values = e.shap_values(x_test[1:5])
+# # explain predictions of the model on three images
+# e = shap.DeepExplainer(model, background)
+# # ...or pass tensors directly
+# # e = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), background)
+# shap_values = e.shap_values(x_test[1:5])
 
-# plot the feature attributions
-shap.image_plot(shap_values, -x_test[1:5])
+# # plot the feature attributions
+# shap.image_plot(shap_values, -x_test[1:5])
